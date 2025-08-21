c8abddd8c5dcdb7d4a470b1cf9f8b67c245f68c4
ndamatta <damatta.natanael@gmail.com>
2025-08-21 16:58:16 -0300

    create card layout, colors and stroke

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <div className="bg-radial from-slate-600 to-slate-950 absolute h-full w-full items-center">
-      <h1>Test</h1>
+    <div className="bg-radial absolute h-full w-full flex items-center justify-center from-slate-600 to-slate-900 ">
+      <div className="bg-slate-100 w-[900px] h-[705px] rounded-[30px] border-x-7 border-indigo-900 outline-2 outline-offset-0 outline-slate-indigo-950">
+        <h1>Hello</h1>
+      </div>
     </div>
   );
 }
