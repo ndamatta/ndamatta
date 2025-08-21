b3e86f00a28051a4a3b5820c6d4d0c62670bb4da
ndamatta <damatta.natanael@gmail.com>
2025-08-21 18:23:57 -0300

    styling tweaks on stroke

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Header from "@/app/components/Header/Header";
 
 export default function HomePage() {
   return (
     <div className="bg-radial absolute h-full w-full flex items-center justify-center from-slate-600 to-slate-900 ">
-      <div className="bg-slate-100 w-[900px] h-[705px] rounded-[30px] border-x-7 border-indigo-900 outline-2 outline-offset-0 outline-slate-indigo-950">
-        <h1>Hello</h1>
+      <div className="bg-slate-100 w-[900px] h-[705px] rounded-[30px] border-x-7 border-indigo-900/80">
+        <Header />
       </div>
     </div>
   );
