5122485f62a7d72a1ff02848146b160544fd4a08
ndamatta <damatta.natanael@gmail.com>
2025-08-22 10:32:03 -0300

    removed circular modularization, animated nav-items

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
-import NavBar from "@/app/components/Header/Navbar";
 
 export default function Header() {
   return (
     <header>
-      <NavBar />
+      <nav>
+        <div className="w-[500px] h-[70px] rounded-b-[50px] bg-indigo-900 justify-self-center content-center shadow-lg">
+          <ul className="flex flex-row justify-evenly text-[23px] text-slate-100 font-bevan">
+            <li className="navitem-animation">
+              <Link href="/career">Career</Link>
+            </li>
+            <li className="navitem-animation">
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li className="navitem-animation">
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
