87da87bd8d22392c03c532a2e2696218aca1ceab
ndamatta <damatta.natanael@gmail.com>
2025-08-27 10:10:39 -0300

    remove default icons, added new styled svg icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,31 +4,18 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer>
-      <div className="w-[210px] h-[40px] rounded-t-[40px] bg-indigo-900 absolute bottom-0 justify-self-center content-center shadow-md">
-        <ul className="flex flex-row justify-evenly text-[23px] text-slate-100 font-bevan translate-y-1 **:rounded-[13px]">
+      <div className="w-[160px] h-[40px] rounded-t-[40px] bg-indigo-900 pt-1 absolute bottom-0 justify-self-center content-center shadow-md">
+        <ul className="flex flex-row justify-evenly text-[23px] text-slate-100 font-bevan **:rounded-[13px]">
           <li className="item-animation">
             <Image
-              src="https://placecats.com/32/32"
+              src="/linkedin.svg"
               width={32}
               height={32}
-              alt="Social media icon"
+              alt="Linkedin icon"
             />
           </li>
           <li className="item-animation">
-            <Image
-              src="https://placecats.com/32/32"
-              width={32}
-              height={32}
-              alt="Social media icon"
-            />
-          </li>
-          <li className="item-animation">
-            <Image
-              src="https://placecats.com/32/32"
-              width={32}
-              height={32}
-              alt="Social media icon"
-            />
+            <Image src="/github.svg" width={32} height={32} alt="Github icon" />
           </li>
         </ul>
       </div>
