efacb07170436df8db221b47a08a93925f204d82
ndamatta <damatta.natanael@gmail.com>
2025-08-27 14:45:39 -0300

    improved header for mobiles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex justify-center mb-10 md:mb-6">
       <nav className="bg-indigo-900 rounded-b-3xl shadow-lg px-8 py-4 w-full max-w-md mx-4">
-        <ul className="flex justify-between text-xl text-slate-100 font-bevan">
+        <ul className="flex justify-between text-md md:text-xl text-slate-100 font-bevan">
           <li className="hover:scale-105 transition-transform">
             <Link href="/career">Career</Link>
           </li>
