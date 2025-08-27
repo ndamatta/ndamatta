b2f4c594808d463be303f904b928d3122878f04b
ndamatta <damatta.natanael@gmail.com>
2025-08-27 14:45:08 -0300

    moved container into a component, worked on normalizing and tweaking font sizes for different vp

---
+++ app/components/CardContainer.tsx
@@ -0,0 +1,28 @@
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+interface CardContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function CardContainer({
+  children,
+  className = "",
+}: CardContainerProps) {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <div
+        className={`bg-slate-100 w-full max-w-4xl flex flex-col rounded-3xl border-4 shadow-2xl border-indigo-900/80 h-[95vh] sm:h-[80vh] ${className}`}
+      >
+        <Header />
+
+        <section className="flex-1 flex flex-col items-center px-6 md:px-16 overflow-y-auto scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-indigo-900 scrollbar-track-slate-100">
+          {children}
+        </section>
+
+        <Footer />
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,35 @@
-import Image from "next/image";
-import Header from "@/app/components/Header";
-import Footer from "@/app/components/Footer";
+import CardContainer from "./components/CardContainer";
 import TechStack from "./components/TechStack";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
-      <div className="bg-slate-100 w-full max-w-4xl min-h-[700px] flex flex-col rounded-3xl border-x-7 relative shadow-2xl border-indigo-900/80">
-        <Header />
-        <section className="flex-1 flex flex-col items-center justify-center px-6 md:px-16">
-          <div className="text-center space-y-6 mb-12">
-            <h1 className="text-3xl md:text-4xl font-bevan">Hi!</h1>
-            <p className="text-lg md:text-xl font-lato max-w-2xl leading-relaxed mx-auto">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
-              vero atque quis necessitatibus sequi autem nihil quae eum
-              voluptatum, delectus facilis ipsa pariatur corporis aliquid iusto
-              assumenda enim nostrum natus? Assumenda, molestiae laborum eveniet
-              placeat tenetur exercitationem illo quidem, suscipit reiciendis
-              praesentium sed ipsam, laboriosam blanditiis mollitia cupiditate
-              voluptatum sint.
-            </p>
-          </div>
-
-          <section className="w-full">
-            <h2 className="text-3xl font-bevan text-center mb-8">Tech Stack</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
-              <TechStack
-                title="Front-end"
-                // icons={["Icon1", "Icon2", "Icon3", "Icon4", "Icon5", "Icon6"]}
-              />
-              <TechStack
-                title="Back-end"
-                // icons={["Icon1", "Icon2", "Icon3", "Icon4", "Icon5", "Icon6"]}
-              />
-              <TechStack
-                title="Tools"
-                // icons={["Icon1", "Icon2", "Icon3", "Icon4", "Icon5", "Icon6"]}
-              />
-            </div>
-          </section>
-        </section>
-        <Footer />
+    <CardContainer>
+      {/* Hero */}
+      <div className="text-center space-y-6 mb-12 mt-8">
+        <h1 className="text-3xl sm:text-3xl md:text-4xl font-bevan text-slate-800">
+          Hi!
+        </h1>
+        <p className="text-base sm:text-lg md:text-xl font-lato max-w-2xl leading-relaxed mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+          praesentium neque excepturi, sint maiores nisi facilis minima commodi
+          ex vero consectetur quas possimus voluptatem deleniti optio molestiae
+          laudantium est tempora assumenda ab provident at. Suscipit minus
+          eligendi qui libero ducimus atque? Quisquam aliquid, similique maiores
+          voluptates illo ullam voluptatum beatae.
+        </p>
       </div>
-    </div>
+
+      {/* Tech Stack */}
+      <section className="w-full">
+        <h2 className="text-2xl sm:text-3xl md:text-3xl font-bevan text-center mb-4 text-slate-800">
+          Tech Stack
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8">
+          <TechStack title="Front-end" />
+          <TechStack title="Back-end" />
+          <TechStack title="Tools" />
+        </div>
+      </section>
+    </CardContainer>
   );
 }
