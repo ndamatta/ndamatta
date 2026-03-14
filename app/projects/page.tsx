e6f96ff1fd62ced81b8cc6ff2140aef08730ba6d
ndamatta <damatta.natanael@gmail.com>
2026-03-14 17:32:45 -0300

    implemented flexible containerWidth, and tweaks in text

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,17 +4,17 @@ import projectsData from "@/data/ProjectData.json";
 
 export default function ProjectsPage() {
   return (
-    <CardContainer>
+    <CardContainer containerWidth="max-w-4xl">
       <section className="flex-1 px-6 md:px-16 py-8 space-y-12">
         <div className="text-center max-w-2xl mx-auto">
           <h1 className="text-3xl sm:text-3xl md:text-3xl font-bevan text-slate-800 mb-4">
             Projects
           </h1>
           <p className="text-base sm:text-lg md:text-xl font-lato text-slate-700 leading-relaxed">
-            A selection of projects I’ve built to explore new technologies,
-            solve real-world problems, and sharpen my skills as a full-stack
-            developer. Each project reflects my focus on clean, efficient code,
-            collaboration, and delivering practical solutions.
+            Some of my personal projects.
+          </p>
+          <p className="text-base sm:text-lg md:text-xl font-lato text-slate-700 leading-relaxed">
+            Some built to solve real problems, and others created to practice and learn new technologies.
           </p>
         </div>
 
