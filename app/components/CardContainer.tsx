e7f74a47527c1546d55d0546f333dd2ab361a1e2
ndamatta <damatta.natanael@gmail.com>
2026-05-29 12:55:22 -0300

    fix: double scroll on mobile version

--- app/components/CardContainer.tsx
+++ app/components/CardContainer.tsx
@@ -15,13 +15,13 @@ export default function CardContainer({
   showFooter = true,
 }: CardContainerProps) {
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="p-4 md:min-h-screen md:flex md:items-center md:justify-center">
       <div
-        className={`bg-slate-100 w-full ${containerWidth} flex flex-col rounded-3xl border-4 shadow-2xl border-indigo-900/80 h-[90vh] ${className}`}
+        className={`bg-slate-100 w-full ${containerWidth} flex flex-col rounded-3xl border-4 shadow-2xl border-indigo-900/80 min-h-[80vh] md:min-h-0 md:h-[90vh] ${className}`}
       >
         <Header />
 
-        <section className="flex-1 flex flex-col items-center px-6 md:px-16 overflow-y-auto scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-indigo-900 scrollbar-track-slate-100">
+        <section className="flex-1 flex flex-col items-center px-6 md:px-16 md:overflow-y-auto md:scrollbar-thumb-rounded-full md:scrollbar-track-rounded-full md:scrollbar md:scrollbar-thumb-indigo-900 md:scrollbar-track-slate-100">
           {children}
         </section>
 
